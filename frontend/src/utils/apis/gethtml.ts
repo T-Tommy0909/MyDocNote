ed82295e0809c2e0482150c11af3e8c0ceff187d
T-Tommy0909 <tomisawa0909tomi@gmail.com>
2023-01-05 13:41:06 +0900

    add split-html

--- frontend/src/utils/apis/gethtml.ts
+++ frontend/src/utils/apis/gethtml.ts
@@ -1,17 +1,26 @@
 import useSWR from "swr";
+import cheerio from "cheerio";
 
 import { requestGet } from "../axios";
 
 export const htmlGet = async (url: string): Promise<any> => {
   const { data } = await requestGet<any>(url);
-  console.log(url);
-  return data;
+
+  const $ = cheerio.load(data);
+  const word_arr: any = [];
+  const separatorString = /[^a-z|^A-Z]+/;
+  $("p,h1,h2,h3,h4", data).each((i, elem) => {
+    word_arr.push($(elem).text().split(separatorString));
+  });
+
+  return word_arr;
 };
 
 export const useHtml = () => {
   const { data, error, mutate } = useSWR(
     ["/html"],
-    async () => await htmlGet("")
+    async () =>
+      await htmlGet("https://www.fastify.io/docs/latest/Reference/Routes/")
   );
   return {
     html: data,
