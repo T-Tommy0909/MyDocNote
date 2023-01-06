77bb79c9687272edc6b40a98d9746fe7f9d8d9a0
T-Tommy0909 <tomisawa0909tomi@gmail.com>
2023-01-07 06:14:58 +0900

    modify gethtml api

--- frontend/src/pages/root.tsx
+++ frontend/src/pages/root.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 
 import { useHtml } from "../utils/apis/gethtml";
+import { WordsBlock } from "../components/root/WordsBlock";
 
 export const Root: FC = () => {
   const { html, isLoading } = useHtml();
@@ -10,7 +11,7 @@ export const Root: FC = () => {
       {!isLoading && (
         <div>
           <div>root page</div>
-          <div>{html}</div>
+          <div>{html && WordsBlock(html)}</div>
         </div>
       )}
     </div>
